5281af39de685e11b40f25b5dada0b74f1ac4013
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-25 20:34:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // console.log("The DOM has loaded");
+    document.getElementById('text').innerHTML = "This is really cool!"
+});
+
+// console.log("JavaScript is so cool. It lets me add text to my page programmatically.");
